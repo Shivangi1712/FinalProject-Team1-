661c511cf73e65d48b3be8aea1283e1fd80e9efc
manishreddy0914 <115566791+manishreddy0914@users.noreply.github.com>
2022-11-02 15:14:14 -0400

    Add files via upload

---
+++ adminhome.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Clocksy Watches</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        .navbar-inverse {
+            background-color: #fff;
+            border-color: #ffffff;
+
+        }
+
+        .navbar-inverse .navbar-nav>.active>a,
+        .navbar-inverse .navbar-nav>.active>a:focus,
+        .navbar-inverse .navbar-nav>.active>a:hover {
+            color: #060606;
+            background-color: #ffffff;
+        }
+
+        .navbar-inverse .navbar-nav>li>a {
+            color: #090909;
+        }
+
+        .navbar-inverse .navbar-nav>li>a:focus,
+        .navbar-inverse .navbar-nav>li>a:hover {
+            color: #0c4ad4;
+            background-color: transparent;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <img src="images/logo.png" style="width: 70px;margin-top: 15px;" />
+                <!-- <a class="navbar-brand" href="#">GSY Services</a> -->
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.php">
+                        <h2>Clocksy Watches</h2>
+                    </a></li>
+
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.php" style="margin-top: 30px;font-size: 20px;">Home</a></li>
+                <li><a href="customers.php" style="margin-top: 30px;font-size: 20px">Customers</a></li>
+                <li><a href="products.php" style="margin-top: 30px;font-size: 20px">Products</a></li>
+                <li><a href="orders.php" style="margin-top: 30px;margin-right: 0px;font-size: 20px">Orders</a></li>
+                 <li><a href="appointments.php" style="margin-top: 30px;font-size: 20px">Appointments</a></li>
+                <li><a href="logout.php" style="margin-top: 30px;margin-right: 100px;font-size: 20px;">Logout</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class="container">
+
+        <div class="row" style="margin-top:100px">
+
+            <div class="col-md-3" >
+                
+                <div class="well" style="text-align:center;padding-top:50px;padding-bottom:50px">
+                    Customers
+                </div>
+                
+            </div>
+
+           
+           <div class="col-md-3" >
+                
+                <div class="well" style="text-align:center;padding-top:50px;padding-bottom:50px">
+                    products
+                </div>
+                
+            </div>
+            
+            <div class="col-md-3" >
+                
+                <div class="well" style="text-align:center;padding-top:50px;padding-bottom:50px">
+                    Orders
+                </div>
+                
+            </div>
+            
+            <div class="col-md-3" >
+                
+                <div class="well" style="text-align:center;padding-top:50px;padding-bottom:50px">
+                    Appointments
+                </div>
+                
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <hr />
+            </div>
+        </div>
+        
+        
+        <div class="row">
+            <div class="col-md-4" style="text-align: center;">
+<ul style="list-style: none;float: center;">
+                    <li style="list-style: none;center: left;padding: 10px;"><a href="#"
+                            style="margin-top: 30px;font-size: 20px;"><u>Terms</u></a></li>
+                 
+
+                </ul>
+            </div>
+
+            <div class="col-md-4" style="text-align: center;">
+                <ul style="list-style: none;float: center;">
+                 
+                    <li style="list-style: none;float: center;padding: 10px;"><a href="#"
+                            style="margin-top: 30px;font-size: 20px;"><u>Privacy Policy</u></a></li>
+                 
+
+                </ul>
+            </div>
+
+            <div class="col-md-4" style="text-align: center;">
+<ul style="list-style: none;float: center;">
+              
+                    <li style="list-style: none;float: center;padding: 10px;"><a href="#"
+                            style="margin-top: 30px;font-size: 20px;"><u>Contact Us</u></a></li>
+
+                </ul>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4" style="text-align: center;font-size: 14px;">
+                Clocksy Towers 557 Boul Henri-BourassaE, Montreal, QC H2C 1E2<br />
+                <span class="glyphicon glyphicon-phone"> Call us @ 514-912-4691</span>
+                <br />
+
+                Copy Right Clocksymanagement Pvt LTD
+<span class="glyphicon glyphicon-envelope"> clocksy123@gmail.com</span>
+                <br />
+
+            </div>
+            <div class="col-md-4">
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+</body>
+
+</html>
