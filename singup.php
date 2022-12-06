2061a47b689bf93edd65e565414edcb207b0fe8e
Gurwindersingh4555 <115567461+Gurwindersingh4555@users.noreply.github.com>
2022-12-05 19:01:39 -0500

    Add files via upload

--- singup.php
+++ singup.php
@@ -9,9 +9,13 @@ $pass = $_GET['pass'];
 $add = $_GET['add'];
 $qus = $_GET['qus'];
 $ans = $_GET['ans'];
+$cno = $_GET['cno'];
+$cvv = $_GET['cvv'];
+$month = $_GET['month'];
+$year = $_GET['year'];
 
 
-//SELECT `id`, `fname`, `lname`, `phone`, `email`, `pass`, `address`, `qus`, `ans`, `status` FROM `user` WHERE 1
+//SELECT `id`, `fname`, `lname`, `phone`, `email`, `pass`, `address`, `qus`, `ans`, `status`, `cardno`, `mon`, `year`, `cvv` FROM `user` WHERE 1
 
 $con=mysqli_connect($hostname, $username, $password,$dbname);
 mysqli_query ($con,"set character_set_results='utf8'");
@@ -21,17 +25,16 @@ $query_json = "SELECT * from user where email='$email'";
             if(!$row)
             {
     
-$query_dis="INSERT INTO user (fname,lname, email,phone, pass,address,qus,ans,status)
-VALUES ('$fname', '$lname', '$email', '$phone','$pass','$add','$qus','$ans','Active')";
+$query_dis="INSERT INTO user (fname,lname, email,phone, pass,address,qus,ans,cardno,mon,year,cvv,status)
+VALUES ('$fname', '$lname', '$email', '$phone','$pass','$add','$qus','$ans','$cno','$month','$year','$cvv','Active')";
 //echo $query_dis; 
 
 $retval_dis = mysqli_query($con,$query_dis);
 
 mysqli_close($con);
- echo '<script> alert("Customer Registerd Successfully")</script>';
+ echo '<script> alert("Customer Register Successfully")</script>';
       echo "<script type='text/javascript'> document.location = 'signin.html';</script>";
 }else{
-      echo '<script> alert("Customer exists please try again")</script>';
       echo "<script type='text/javascript'> document.location = 'singup.html';</script>";
 }
 ?>
